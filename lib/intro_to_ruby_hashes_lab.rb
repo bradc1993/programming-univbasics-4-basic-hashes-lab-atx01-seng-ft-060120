2a39ebf0d9428abbb2dda8f8b70729c983665870
Brad Carter <bradc1993@gmail.com>
2020-05-07 00:17:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-  
+  { :color => "green" }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  { name: "Grace Hopper" }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  { id: number }
 end
